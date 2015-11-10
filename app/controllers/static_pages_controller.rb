7d1fb6daeeb4b6879778a9105c97cd07971c7d27
SergeyMorugin <cmorugin@gmail.com>
2015-11-10 12:31:41 +0300

    Make execute function

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,16 +2,32 @@ require 'net/ssh'
 require 'net/scp'
 
 class StaticPagesController < ApplicationController
-
+  @@index = 0
 
   def home
   end
 
   def about
-    Resque.enqueue(ImageJob)
+    #@@index += 1
+    #Resque.enqueue(ImageJob, @@index)
         #ProcessImageJob.perform_later "asd"
+    process_image
   end
 
   def error
   end
+
+  protected
+
+  def process_image
+    pri = ImageJob.new
+    @result = pri.execute
+    #if pri.execute
+     # @result = "OK"
+    #else
+    #  @result = "Error"
+    #end
+
+  end
+
 end
