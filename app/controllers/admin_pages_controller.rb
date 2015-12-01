301d50a849cdead7428e1d10418b353847517a03
SergeyMorugin <cmorugin@gmail.com>
2015-12-01 14:22:37 +0300

    Same design fix

--- app/controllers/admin_pages_controller.rb
+++ app/controllers/admin_pages_controller.rb
@@ -17,7 +17,7 @@ class AdminPagesController < ApplicationController
   end
 
   def images
-    @items= QueueImage.all.order('created_at DESC').paginate(:page => params[:page], :per_page => 10)
+    @items= QueueImage.all.order('updated_at DESC').paginate(:page => params[:page], :per_page => 10)
   end
 
   def users
